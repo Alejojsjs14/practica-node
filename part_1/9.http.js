f74bef102d08ab8a72b2090405ce7d4087ef0d4d
Alejandro Molina <alejandro.ytf@gmail.com>
2024-11-18 20:04:25 +0000

    Nose que pasho

--- part_1/9.http.js
+++ part_1/9.http.js
@@ -1,17 +1,15 @@
 const http = require('node:http')
-const { findAvailablePort } = require('./10.free-port')
+const { findAvailablePort } = require('./10.free-port.js')
 
-console.log(process.env)
-
-const desiredPort = process.env.PORT ?? 3000
+const desiredPort = process.env.PORT ?? 1234
 
 const server = http.createServer((req, res) => {
-  console.log('Reques recived')
-  res.end('hola mundo')
+  console.log('request received')
+  res.end('Hola mundo')
 })
 
 findAvailablePort(desiredPort).then(port => {
   server.listen(port, () => {
-    console.log(`server listening on port http://localhost:${port}`)
+    console.log(`Server listening on port http://localhost:${port}`)
   })
 })
