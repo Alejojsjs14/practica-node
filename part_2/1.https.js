1fe1ea1377aeffd8ce4ce80d8c36433a314c2e4b
Alejandro Molina <alejandro.ytf@gmail.com>
2024-11-20 22:25:29 +0000

    feature: ejemplo api funcional desde 0 con node

--- part_2/1.https.js
+++ part_2/1.https.js
@@ -1,33 +1,37 @@
 const http = require('node:http')
 const fs = require('node:fs')
 
-const desiredPort = process.env.PORT ?? 3000
+const desirePort = process.env.PORT ?? 1234
 
-const processRequest = (req, res) => {
-  res.setHeader('Content-Type', 'text/html; charset=utf-8')
-
+const procesRequest = (req, res) => {
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8')
   if (req.url === '/') {
-    res.end('<h1>Bienvenido a mi página de inicio</h1>')
-  } else if (req.url === '/imagen-bonita.png') {
+    res.statusCode = 200
+    res.setHeader('Charset', 'utf-8')
+    res.end('<h1>Bienvenido a mi pagina de inicion<h1>')
+  } else if (req.url === '/imagen') {
     fs.readFile('./Camon_20.png', (err, data) => {
       if (err) {
         res.statusCode = 500
-        res.end('<h1>internal server error</h1>')
+        res.end('<h1>500 internal server error<h1>')
       } else {
         res.setHeader('Content-Type', 'image/png')
         res.end(data)
       }
     })
   } else if (req.url === '/contacto') {
-    res.end('<h1>Contacto</h1>')
+    res.statusCode = 200
+    res.setHeader('Charset', 'utf-8')
+    res.end('<h1>Contacto<h1>')
   } else {
-    res.statusCode = 404 // Not found
-    res.end('<h1>404</h1>')
+    res.statusCode = 404
+    res.end('<h1>404<h1>')
   }
 }
 
-const server = http.createServer(processRequest)
+const server = http.createServer(procesRequest)
 
-server.listen(desiredPort, () => {
-  console.log(`server listening on port http://localhost:${desiredPort}`)
+server.listen(desirePort, () => {
+  console.log(`server listening on port http://localhost:${desirePort}`)
 })
+ 
