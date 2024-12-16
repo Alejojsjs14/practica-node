7c884850eacfdfb47f219437bc6d6e24a99ca514
Alejandro Molina <alejandro.ytf@gmail.com>
2024-12-16 16:46:44 -0500

    feat: organizando la parte 2 y integrando funcionalidades de express a los servicios

--- part_2/3.express.js
+++ part_2/3.express.js
@@ -1,30 +1,58 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const ditto = require("./pokemon/dito.json");
 
-const PORT = process.env.PORT ?? 1234
+const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).send('<h1>Mi página</h1>')
-})
+const PORT = process.env.PORT ?? 1234;
 
-app.post('/pokemon', (req, res) => {
-  let body = "";
+// deshabilita la tecnologia usada ejem: express
+app.disable("x-powered-by");
 
-  // escuchar el evento data
-  req.on("data", (chunk) => {
-    body += chunk.toString();
+app.use(express.json());
+
+// // Creando en un middleware
+// app.use((req, res, next) => {
+//   if (req.method !== "POST") return next();
+//   if (req.headers["content-type"] !== "application/json") return next();
+
+//   //Solo llegan reques post y que tienen headers de content type application JSON
+
+//   let body = "";
+
+//   // escuchar el evento data
+//   req.on("data", (chunk) => {
+//     body += chunk.toString();
+//   });
+
+//   req.on("end", () => {
+//     const data = JSON.parse(body);
+//     data.timestamp = Date.now();
+//     //mutar la request y meter la informacion en el req.body
+//     req.body = data;
+//     next();
+//   });
+// });
+
+app.get("/", (req, res) => {
+  res.status(200).send("<h1>Mi página</h1>");
+});
+
+app.post("/pokemon", (req, res) => {
+  //req.body deberiamos guardar en bd
+  res.status(201).json(req.body);
+});
+
+app.get("/pokemon/ditto", (req, res) => {
+  res.status(200).json({
+    message: "Data success",
+    ditto,
   });
+});
 
-  req.on("end", () => {
-    const data = JSON.parse(body);
-    res.writeHead(201, {
-      "Content-Type": "application/json; charset=utf-8",
-    });
-    res.end(JSON.stringify(data));
-  });
-
-})
+app.use((req, res) => {
+  res.status(404).send("<h1>404 page not found</h1>");
+});
 
 app.listen(PORT, () => {
-  console.log(`server listening on port http://localhost:${PORT}`)
-})
+  console.log(`server listening on port http://localhost:${PORT}`);
+});
