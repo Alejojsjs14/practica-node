f1b837cb6e3504d5125eb3e96cf07616107c3b46
Alejandro Molina <alejandro.ytf@gmail.com>
2024-12-24 07:23:42 -0500

    implementacion parte 3 (no terminada aun)

--- part_3/app.js
+++ part_3/app.js
@@ -1,15 +1,59 @@
 const express = require('express')
+const crypto = require('node:crypto')
 const movies = require('./movies.json')
+const { validateMovie } = require('./schemas/movies')
 
 const app = express()
+app.use(express.json())
 app.disable('x-powered-by')
 
-app.get('/', (req, res) => {
-  res.json({ message: 'hola mundo' })
+// Todo lo que sea movies se indetifica con /movies
+app.get('/movies', (req, res) => {
+  const { genre } = req.query
+  if (genre) {
+    const filteredMovies = movies.filter((movie) =>
+      movie.genre.some((g) => g.toLowerCase() === genre.toLowerCase())
+    )
+    return res.json(filteredMovies)
+  }
+
+  res.status(200).json(movies)
 })
 
-app.get('/movies', (req, res) => {
-  res.status(200).json(movies)
+// recuperar peliculas por id
+app.get('/movies/:id', (req, res) => {
+  // path-to-regexp
+  const { id } = req.params
+
+  const movie = movies.find((movie) => movie.id === id)
+  if (movie) return res.status(200).json(movie)
+
+  res.status(404).json({ error: 'Movie not found' })
+})
+
+app.post('/movies', (req, res) => {
+  const result = validateMovie(req.body) // utiliza zod para validar los datos del body
+
+  // devuelve un objeto con un array de errores
+  if (result.error) {
+    res.status(400).json({ error: result.error.errors })
+  }
+
+  // genera un nueva objeto con la data validada
+  const newMovie = {
+    id: crypto.randomUUID(),
+    ...result.data // los trespuntos es para desestructurar el objeto
+  }
+
+  movies.push(newMovie)// agrega la nueva pelicula al array de peliculas
+
+  res.status(201).json(newMovie)
+})
+
+app.patch('/movies/:id', (req, res) => {
+  const { id } = req.params
+
+  
 })
 
 const PORT = process.env.PORT ?? 1234
--- part_3/schemas/movies.js
+++ part_3/schemas/movies.js
@@ -1,26 +1,30 @@
 const z = require('zod')
 
-const movieSchema = z.string({
+// se utiliza zod para validar los datos del body
+const movieSchema = z.object({
   title: z.string({
     invalid_type_error: 'Movie title must be a string',
-    required_error: 'movie title is required'
+    required_error: 'Movie title is required'
   }),
-  year: z.number().int().min(1900).max(2024),
+  year: z.number().int().min(1900).max(2025),
   director: z.string(),
   duration: z.number().int().positive(),
-  rate: z.number().min(0).max(10),
+  rate: z.number().min(0).max(10).default(5), // la propiedad puede ser default(5.5) / optional()
   poster: z.string().url({
-    message: 'must be a valid url'
+    message: 'Movie poster must be a valid URL'
   }),
-  genre: z.array(
-    z.enum(['Action', 'Terror', 'Comedy', 'Drama']), {
-      required_error: 'Movie genre is required'
-    }
-  )
+  genre: z.array(z.enum(['Action', 'Adventure', 'Comedy', 'Drama', 'Horror', 'Mystery', 'Thriller']), {
+    required_error: 'Movie genre is required',
+    invalid_type_error: 'Movie genre must be an array of strings'
+  })
 })
 
-function validateMovie (objec) {
-  return movieSchema.safeParse(objec)
+function validateMovie (object) {
+  return movieSchema.safeParse(object)
+}
+
+function validatePartialMovie (object) {
+  return movieSchema.partial().safeParse(object)
 }
 
 module.exports = {
